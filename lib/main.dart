b86de359635e45eb7e9f6eee210e42ae2a0e7b9a
OmarIssa0 <omar.issa.slwade@gmail.com>
2024-05-25 21:54:46 +0300

    splash view logic done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/helper_function/on_generate_routes.dart';
 import 'package:fruit_hub/features/splash/presentation/view/splash_view.dart';
 
 void main() {
@@ -11,7 +12,9 @@ class FruitHub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      onGenerateRoute: ,
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
     );
   }
 }
